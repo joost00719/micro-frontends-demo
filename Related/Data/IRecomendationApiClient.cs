f141417b17ab9ea39f1c0429be447f161d6cc7bc
Joost Roza <joost@roza.dev>
2023-06-16 13:34:52 +0200

    fixes

--- Related/Data/IRecomendationApiClient.cs
+++ Related/Data/IRecomendationApiClient.cs
@@ -10,26 +10,4 @@ namespace Related.Data
     {
         public Task<Recommendation> GetRecommendationAsync(int tractorId);
     }
-
-    internal class RecomendationApiClient : IRecomendationApiClient
-    {
-        public async Task<Recommendation> GetRecommendationAsync(int tractorId)
-        {
-            if (tractorId == 1)
-            {
-                return new Recommendation { Image1 = "reco_3.jpg", Image2 = "reco_5.jpg", Image3 = "reco_6.jpg" };
-            }
-            else if (tractorId == 2)
-            {
-                return new Recommendation { Image1 = "reco_3.jpg", Image2 = "reco_6.jpg", Image3 = "reco_4.jpg" };
-
-            }
-            else if (tractorId == 3)
-            {
-                return new Recommendation { Image1 = "reco_1.jpg", Image2 = "reco_8.jpg", Image3 = "reco_7.jpg" };
-            }
-
-            return null;
-        }
-    }
 }

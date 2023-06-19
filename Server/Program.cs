203b6953aec26abc4952f9154ebd27e6bf749b00
Joost Roza <joost@roza.dev>
2023-06-19 10:15:25 +0200

    Removed some references which were removed in the previous commit

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Server.Data;
 using Product;
 
 namespace Server
@@ -14,7 +13,6 @@ namespace Server
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
-            builder.Services.AddSingleton<WeatherForecastService>();
 
             // Add the Product frontend
             builder.Services.AddProductUI();

ba2228415c035d602a8cf7bddba51d42ab4e214f
Joost Roza <joost@roza.dev>
2023-10-26 12:16:11 +0200

    Add project files.

---
+++ Server/Pages/TractorStore.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Server.Pages
+{
+    public partial class TractorStore
+    {
+        public TractorStore()
+        {
+
+        }
+    }
+}
